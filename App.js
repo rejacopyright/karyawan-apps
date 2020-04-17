fa1d6bd4c8c692532c04206d25f2b779d59dc4d7
rejajamil <reja.copyright@gmail.com>
2020-04-17 16:33:47 +0700

    update

--- App.js
+++ App.js
@@ -1,51 +1,41 @@
 import React, { Component, Fragment } from 'react';
-import {YellowBox} from 'react-native';
+import { Grid, Row, Col } from "react-native-easy-grid"
 import { NativeRouter, Switch, Route, Link } from "react-router-native";
-import { Text, View, Image, StyleSheet, TextInput } from 'react-native';
+import { Text, Image, TextInput, StatusBar } from 'react-native';
+import St from './src/assets/styles/index'
 import axios from 'axios';
 import con from './src/api/api';
 // ROUTE
 import Home from './src/pages/home'
+import Absen from './src/pages/absen'
+import Profile from './src/pages/profile/index'
 import PageNotFound from './src/pages/pageNotFound'
 // import fire from './src/con/fire';
-const styles = StyleSheet.create({
-  shadowTop: {
-    borderTopWidth:1,
-    borderColor: '#fafafa',
-    // borderRadius: 20,
-    elevation: 10
-  },
-  shadowBottom: {
-    // borderTopWidth:0,
-    borderColor: 'rgba(255,255,255,0)',
-    // borderRadius: 20,
-    elevation: 5
-  },
-});
 class Footer extends Component {
   render() {
     const menu = [
       { name: 'Home', icon: require('./src/icons/active/home.png'), uri: 'home' },
       { name: 'Orders', icon: require('./src/icons/active/briefcase.png'), uri: 'orders' },
-      { name: 'Search', icon: require('./src/icons/active/search.png'), uri: 'search' },
+      { name: 'Absen', icon: require('./src/icons/active/finger.png'), uri: 'absen' },
       { name: 'Inbox', icon: require('./src/icons/active/folder.png'), uri: 'inbox' },
-      { name: 'Account', icon: require('./src/icons/active/user.png'), uri: 'account' }
+      { name: 'Profile', icon: require('./src/icons/active/user.png'), uri: 'profile' }
     ]
     return (
-      <View style={[{ height: 60, backgroundColor: '#fff', flexDirection: 'row', alignItems: 'center' }, styles.shadowTop]}>
+      <Row style={[St.shadowTop, { height: 60, backgroundColor: '#fff' }]}>
+        <StatusBar hidden={false} barStyle="dark-content" backgroundColor="#f5f5f5" />
         {
           menu.map((r, key) => (
-            <Link to={r.uri} key={key} style={{ flex: 1, alignItems: 'center' }} underlayColor="rgba(0,0,0¸0)">
-              <View>
-                <View style={{ height:30, width:30 }}>
-                  <Image source={r.icon} style={{ resizeMode: "contain", width: '100%', height: '100%' }}></Image>
-                </View>
-                <Text style={{ fontSize: 10, color: '#555', marginTop: 2 }}>{r.name}</Text>
-              </View>
-            </Link>
+            <Col key={key} style={[St.center]}>
+              <Link to={r.uri} underlayColor="rgba(0,0,0¸0)" style={[St.center]}>
+                <>
+                  <Image source={r.icon} style={{ resizeMode: "contain", width: 30, height: 30 }}></Image>
+                  <Text style={[St.textCenter, St.f10]}>{r.name}</Text>
+                </>
+              </Link>
+            </Col>
           ))
         }
-      </View>
+      </Row>
     );
   }
 }
@@ -64,15 +54,17 @@ class App extends Component {
   render() {
     return (
       <NativeRouter>
-        <View style={{ flex: 1 }} key={1}>
+        <Col>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/absen" component={Absen} />
             <Route exact path="/home" component={Home} />
+            <Route exact path="/profile" component={Profile} />
             <Route component={PageNotFound} />
           </Switch>
           {/* --- Footer --- */}
           <Footer />
-        </View>
+        </Col>
       </NativeRouter>
     );
   }
